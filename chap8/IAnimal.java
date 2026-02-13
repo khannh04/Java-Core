8703f25aedfff0c6498c046b16dedd93ba77d8c3
khannh <kn213742@gmail.com>
2026-02-13 22:57:16 +0700

    13/2

---
+++ chap8/IAnimal.java
@@ -0,0 +1,6 @@
+package chap8;
+// interface method (does not have a body)
+public interface IAnimal {
+    public void animalSound();
+    public void sleep();
+}
---
+++ chap8/Pig.java
@@ -0,0 +1,9 @@
+package chap8;
+
+public class Pig implements IAnimal{
+    public void animalSound( ) {// The body of animalSound( ) is provided here
+        System.out.println("The pig says: wee wee");
+    }
+    public void sleep( ) {// The body of sleep( ) is provided here
+        System.out.println("Zzz"); }
+}
